1a2b0e4187f596f7b3ef934e24f78e8eeb692977
Jorge Oliveira <jorgeoliveira2k22@gmail.com>
2025-11-02 16:07:01 +0000

    Sidebar

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,38 @@
+"use client";
+
+import Image from "next/image";
+import { PlusCircle } from "lucide-react";
+import { useUser } from "@clerk/clerk-react";
+import { Button } from "@/components/ui/button";
+
 const DocumentsPage = () => {
-  return <div>This is a protected page</div>;
+  const { user } = useUser();
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/lightempty3.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/darkempty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to {user?.firstName}&apos;s Cerebra
+      </h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
